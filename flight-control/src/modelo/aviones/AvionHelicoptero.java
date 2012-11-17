416e10b9c4c9a8411317f37792ae23c41ec58e3f
carlosdariocampos <carlosdariocampos@hotmail.com>
2012-11-17 23:34:57 +0000

    Hay arregle unas cosas de malas referencias que quedaron mal en el primer commit y puse unas pruebas

---
+++ flight-control/src/modelo/aviones/AvionHelicoptero.java
@@ -0,0 +1,57 @@
+package modelo.aviones;
+
+import modelo.general.Posicion;
+import modelo.movimientos.Movimiento;
+import modelo.movimientos.MovimientoSimple;
+import modelo.pistas.Pista;
+
+public class AvionHelicoptero extends Avion {
+
+	private boolean senalDeAlto = false; 
+	
+	private AvionHelicoptero(Posicion posicion, Integer radio) {
+		super(posicion, radio);
+
+	}
+	public static AvionHelicoptero crearAvionHelicopteroSimple(Posicion posicion,Integer radio, Integer velocidad, Double direccion){
+		AvionHelicoptero avion = new AvionHelicoptero(posicion, radio);
+		Movimiento mov = new MovimientoSimple(velocidad, direccion);
+		avion.setMovimiento(mov);
+		return avion;
+	}
+	
+	public void vivir() throws Exception{
+		if (!senalDeAlto) {
+			this.realizarElMovimiento();
+		}
+		else {
+			paraAvion();
+		}
+		
+	}
+	
+	
+	private void paraAvion()
+	{
+		Movimiento movimientoParado = new MovimientoSimple (0,this.getMovimiento().getDireccion());
+		this.setMovimiento(movimientoParado);
+	}
+	
+	
+	public void setOnSenalDeFreno()
+	{
+		senalDeAlto= true;
+	}
+
+	public void setOffSenalDeFreno()
+	{
+		senalDeAlto= false;
+	}
+	
+
+	public Boolean intentarAterrizar(Pista pista) {
+
+		return pista.puedeAterrizar(this);
+	}
+
+}
